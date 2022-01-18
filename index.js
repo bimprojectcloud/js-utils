5b4b3b0f7d641d8f5ae54d484ce4218f31c7f264
Lukas Tomcik <lukas.tomcik@bimproject.cz>
2022-01-18 10:13:51 +0100

    fix: export localize function

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("./express");
 const fs = require("./fs");
+const localize = require("./localize");
 const statusServer = require("./statusServer");
 
 module.exports = {
     express,
     fs,
+    localize,
     statusServer,
 };
